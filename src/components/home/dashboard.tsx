4c88881d1e6b3b5d4e7cedb6b8e6da8f45a746f7
Shivam Patel <smpatel0423@gmail.com>
2025-07-27 20:18:13 -0400

    fix

--- src/components/home/dashboard.tsx
+++ src/components/home/dashboard.tsx
@@ -79,7 +79,7 @@ export default function Dashboard() {
             className="flex items-center justify-center overflow-hidden rounded-2xl border-2 bg-background"
             gradientColors={["#A97CF9E6", "#F38CB9E6", "#FDCC92E6"]}
           >
-      <p className="text-7xl">😎</p>
+      <p className="text-8xl">😎</p>
     </ScratchToReveal>
         </GridItem>
         <GridItem
