f13b52bc308b413fb408df62823ad2eb0e62e067
Shivam Patel <smpatel0423@gmail.com>
2025-07-30 01:04:33 -0400

    rainbow button

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,15 +1,26 @@
 "use client";
 
 import Image from "next/image";
+import { RainbowButton } from "@/components/magicui/rainbow-button";
 
 export const Footer = () => {
+
     return (
-        <footer className="relative w-full sm:h-[25rem] h-[10rem] bg-background text-secondary-foreground pt-4 overflow-hidden">
+        <footer className="relative w-full sm:h-[30rem] h-[20rem] bg-background text-secondary-foreground pt-4 overflow-hidden">
+
             <div className="container relative z-10 mx-auto flex flex-col h-full items-center justify-end px-4 py-4">
-                <p className="text-sm text-center">
+                <div className="flex flex-col items-center justify-center h-full">
+                    <h2 className="text-center text-pretty text-xl sm:text-2xl font-bold mb-8">
+                        Have any questions, or just want to chat?
+                    </h2>
+                    <a href="mailto:shivypat@gmail.com">
+                        <RainbowButton className="bg-background">Contact Me</RainbowButton>
+                    </a>
+                </div>
+                <p className="text-sm text-center font-semibold">
                     &copy; {new Date().getFullYear()} Shivam Patel. All rights reserved.
                 </p>
-                <p className="text-xs text-center align-text-bottom mt-2">
+                <p className="text-xs text-center align-text-bottom font-semibold mt-2">
                     Built with ❤️ using React, Next.js and Tailwind
                 </p>
             </div>
